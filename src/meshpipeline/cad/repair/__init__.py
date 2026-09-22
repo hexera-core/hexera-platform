bf68637655966c0cac4b4bc90f646bb8212e5553
Pranav Kannepalli <kitts@Pranavs-MacBook-Pro-472.local>
2026-09-22 00:11:53 -0700

    Add CAD repair report contracts

---
+++ src/meshpipeline/cad/repair/__init__.py
@@ -0,0 +1,31 @@
+"""Diagnostics-only CAD repair core."""
+
+from meshpipeline.cad.repair.contracts import (
+    DefectCode,
+    DefectSeverity,
+    RepairDefect,
+    RepairInput,
+    RepairMeasurement,
+    RepairMode,
+    RepairPolicy,
+    RepairProfile,
+    RepairReport,
+    RepairResult,
+    RepairStatus,
+    RepairTarget,
+)
+
+__all__ = [
+    "DefectCode",
+    "DefectSeverity",
+    "RepairDefect",
+    "RepairInput",
+    "RepairMeasurement",
+    "RepairMode",
+    "RepairPolicy",
+    "RepairProfile",
+    "RepairReport",
+    "RepairResult",
+    "RepairStatus",
+    "RepairTarget",
+]
